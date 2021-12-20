e5e7f8639697a69ec15349a9d8d8149478c81166
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-12-20 17:07:10 +0100

    implemented automatic program initialization given an array of socket ports

--- src/main/java/part2/Main.java
+++ src/main/java/part2/Main.java
@@ -2,32 +2,43 @@ package part2;
 
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.remote.RemoteTransportException;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import part2.actor.GameActor;
 import part2.message.Message;
 import part2.view.PuzzleBoard;
 
+import java.util.Optional;
+
 public class Main {
 
     public static void main(final String[] args) {
-        final int n = 3;
-        final int m = 5;
+        final int n = 2;
+        final int m = 2;
 
         final String imagePath = "src/main/resources/bletchley-park-mansion.jpg";
 
-        // load akka clustering configuration
-        int port = 25252;
-        Config config = ConfigFactory
-                .parseString("akka.remote.artery.canonical.port="+port)
-                .withFallback(ConfigFactory.load("application_cluster"));
+//        int[] ports = {25252, 25253, 25254};
+        int[] ports = {25252};
 
-        final ActorSystem<Message> system =
-                ActorSystem.create(Behaviors.empty(), "game", config);
+        for(int p : ports) {
+            try {
+                Config config = ConfigFactory
+                        .parseString("akka.remote.artery.canonical.port=" + p)
+                        .withFallback(ConfigFactory.load("application_cluster"));
+                final ActorSystem<Message> system =
+                        ActorSystem.create(Behaviors.empty(), "game", config);
 
-        final PuzzleBoard puzzle = new PuzzleBoard(n, m, imagePath, system);
-        puzzle.setVisible(true);
+                final PuzzleBoard puzzle = new PuzzleBoard(n, m, imagePath, system);
+                puzzle.setVisible(true);
 
+                System.out.println("Initialized cluster actor at port " + p);
+                break;
+            } catch (RemoteTransportException e) {
+                System.out.println("Unable to initialize a distributed actor. Port " + p + " already occupied by another process.");
+                // e.printStackTrace();
+            }
+        }
     }
-
 }
